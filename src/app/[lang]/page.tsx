663c1432b09459119b5cbb2da1ac6b90493916c9
phucdh410 <phucdh410@gmail.com>
2023-07-21 09:11:00 +0700

    Add[Metadata]: add metadata example

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,4 +1,6 @@
 // import { getDictionary } from "@/dictionaries";
+import { Metadata } from "next";
+
 import {
   MCommonData,
   MCooperation,
@@ -10,8 +12,32 @@ import { IPageProps } from "@/types/page";
 
 interface IHomePageProps extends IPageProps {}
 
+const getData = async () => {
+  const res = await fetch(
+    "https://axitamin.com.vn/api/knowledges/getBySlug/haha"
+  );
+
+  if (!res.ok) throw new Error("Lỗi !!!");
+
+  return res.json();
+};
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const data = await getData();
+
+  return {
+    title: data.data.title,
+    description: "Something",
+    openGraph: {
+      images:
+        "https://cdn.tuoitre.vn/471584752817336320/2023/7/20/xe-dien-o-chau-au-anh-counterpointresearch-16898241932271499450977.jpg",
+    },
+  };
+};
+
 export default async function Home({ params }: IHomePageProps) {
   // const d = await getDictionary(params.lang)
+  const data = await getData();
 
   return (
     <>
