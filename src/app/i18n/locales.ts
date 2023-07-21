1a75896035f7999be461fb8bd046c8ddf3d18bac
phucdh410 <phucdh410@gmail.com>
2023-07-21 16:28:01 +0700

    Update[Button]: update UI switch langs button

--- src/app/i18n/locales.ts
+++ src/app/i18n/locales.ts
@@ -1,6 +1,6 @@
 export const LANGUAGES = [
-  { id: "1", name: "VI", value: 1, locale: "vi" },
-  { id: "2", name: "EN", value: 2, locale: "en" },
+  { id: "1", name: "VI", value: 1, locale: "vi", flag: "images/vi.png" },
+  { id: "2", name: "EN", value: 2, locale: "en", flag: "images/en.png" },
 ];
 export const defaultLocale = "vi";
 export const locales = [defaultLocale, "en"];
--- src/common/components/controls/CSwitchLanguageButton/index.tsx
+++ src/common/components/controls/CSwitchLanguageButton/index.tsx
@@ -5,7 +5,7 @@ import { usePathname, useRouter } from "next-intl/client";
 
 import { useCallback, useState } from "react";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { defaultLocale, LANGUAGES } from "@/app/i18n";
 import { useClickOutside } from "@/utils/hooks";
@@ -48,30 +48,40 @@ export const CSwitchLanguageButton = () => {
   return (
     <span className="relative cursor-pointer" onClick={open}>
       <CLanguageIcon />
-      {isOpen && (
-        <motion.div
-          ref={divRef}
-          className="absolute left-1/2 -translate-x-1/2 translate-y-1 bg-white rounded-md p-1 shadow-[0_0_10px_6px_rgb(0_0_0_/_20%)]"
-        >
-          <ul>
-            {LANGUAGES.map((e) => (
-              <li key={e.id} className="text-center">
-                <button
-                  onClick={onSelectLang}
-                  className={`px-3 py-1 w-full hover:bg-[rgb(0_0_0_/_6%)] text-sm ${
-                    locale === e.locale
-                      ? "text-red font-bold"
-                      : "text-sub font-medium"
-                  }`}
-                  value={e.value}
-                >
-                  {e.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            ref={divRef}
+            key="switch-button"
+            initial={{ y: -20, x: "-50%", opacity: 0 }}
+            animate={{ y: 5, x: "-50%", opacity: 1 }}
+            exit={{ y: -5, opacity: 0 }}
+            className="absolute left-1/2 -translate-x-1/2 translate-y-1 bg-white rounded-md p-1 shadow-[0_0_10px_6px_rgb(0_0_0_/_20%)]"
+          >
+            <ul>
+              {LANGUAGES.map((e) => (
+                <li key={e.id} className="text-center">
+                  <button
+                    onClick={onSelectLang}
+                    className={`px-2 py-1 w-full flex items-center justify-start gap-2 
+                    ${
+                      locale === e.locale && "bg-slate-200"
+                    } hover:bg-[rgb(0_0_0_/_6%)] text-sm ${
+                      locale === e.locale
+                        ? "text-red font-bold"
+                        : "text-sub font-medium"
+                    }`}
+                    value={e.value}
+                  >
+                    <img src={e.flag} alt="" className="max-w-[16px] h-auto" />
+                    {e.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </span>
   );
   //#endregion
