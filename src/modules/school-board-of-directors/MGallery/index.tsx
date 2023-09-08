03129c0e921613195adf51cfc3661bf00f208ac7
phucdh410 <phucdh410@gmail.com>
2023-09-08 10:11:02 +0700

    Update[BanGiamHieu]: update responsive

--- src/modules/school-board-of-directors/MGallery/index.tsx
+++ src/modules/school-board-of-directors/MGallery/index.tsx
@@ -31,16 +31,20 @@ const MOCK = [
 
 export const MGallery = () => {
   return (
-    <div className="container mt-header mb-20 grid gap-5 grid-cols-3 select-none">
-      <div className="col-span-3 relative">
+    <div className="container mt-header mb-20 grid gap-5 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 select-none">
+      <div className="col-span-1 md:col-span-2 2xl:col-span-3 relative">
         <img src={MOCK[0].image} alt="" />
-        <div className="absolute left-[90px] top-1/2 -translate-y-1/2 text-white">
-          <h3 className="font-serif4 font-bold tracking-1.2px text-40px">
+        <div className="absolute left-5 2xl:left-[90px] top-1/2 -translate-y-1/2 text-white">
+          <h3 className="font-serif4 font-bold tracking-1.2px text-clamp40px">
             {MOCK[0].name}
           </h3>
-          <div className="my-divider mt-10px mb-7 min-h-[1px] w-full bg-[linear-gradient(90deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_100%)]"></div>
-          <p className="leading-21px tracking-0.48px">{MOCK[0].position}</p>
-          <p className="leading-21px tracking-0.48px">{MOCK[0].term}</p>
+          <div className="my-divider mt-10px mb-3 2xl:mb-7 min-h-[1px] w-full bg-[linear-gradient(90deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_100%)]"></div>
+          <p className="text-clamp16px leading-21px tracking-0.48px">
+            {MOCK[0].position}
+          </p>
+          <p className="text-clamp16px leading-21px tracking-0.48px">
+            {MOCK[0].term}
+          </p>
         </div>
       </div>
       {MOCK.slice(1).map((item) => (
@@ -50,8 +54,12 @@ export const MGallery = () => {
             <h4 className="text-xl font-semibold tracking-0.64px font-serif4 mb-4">
               {item.name}
             </h4>
-            <p className="leading-21px tracking-0.48px">{item.position}</p>
-            <p className="leading-21px tracking-0.48px">{item.term}</p>
+            <p className="text-clamp16px leading-21px tracking-0.48px">
+              {item.position}
+            </p>
+            <p className="text-clamp16px leading-21px tracking-0.48px">
+              {item.term}
+            </p>
           </div>
         </div>
       ))}
