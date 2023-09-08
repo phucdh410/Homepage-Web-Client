331dba9a0b39b16a0784bfa8c4315647c82d5935
phucdh410 <phucdh410@gmail.com>
2023-09-08 17:35:00 +0700

    Update[Animation]: update anim

--- src/modules/council/MMembers/MPersonInfo/index.tsx
+++ src/modules/council/MMembers/MPersonInfo/index.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 import { IMPersonInfoProps } from "./types";
 
 export const MPersonInfo = ({ data }: IMPersonInfoProps) => {
   return (
-    <div className="flex flex-col gap-2 md:gap-5">
+    <motion.div
+      initial={{ y: 200, opacity: 0 }}
+      whileInView={{
+        y: 0,
+        opacity: 1,
+        transition: { duration: 0.8, bounce: 0.3, type: "spring" },
+      }}
+      viewport={{ once: true }}
+      className="flex flex-col gap-2 md:gap-5"
+    >
       <img src={data.image} alt="" className="object-cover rounded-2xl" />
       <div className="px-2 md:px-5">
         <div className="mb-2 md:mb-4 text-primary font-serif4 font-semibold text-xl">
@@ -10,6 +23,6 @@ export const MPersonInfo = ({ data }: IMPersonInfoProps) => {
         </div>
         <div className="text-black text-16px font-normal">{data.position}</div>
       </div>
-    </div>
+    </motion.div>
   );
 };
